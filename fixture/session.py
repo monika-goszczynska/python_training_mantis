353f2412b596eca9c4a95dbe88a01a07de80efa7
monika-goszczynska <monika.gx@gmail.com>
2021-05-26 08:44:35 +0200

    fixture project

--- fixture/session.py
+++ fixture/session.py
@@ -31,7 +31,6 @@ class SessionHelper:
     def get_logged_user(self):
         wd = self.app.wd
         return wd.find_element_by_id("logged-in-user").text
-        # return wd.find_element_by_css_selector("td.login-info-left span").text
 
     def ensure_logout(self):
         wd = self.app.wd
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,3 +1,8 @@
 
 def test_add_project(app):
     pass
+    #click manage
+    #click manage projects
+    #click create new project
+    #enter project name
+    #click add project
